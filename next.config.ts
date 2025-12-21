2faba4a8978a1e831d86dd4880eedf94cb9f5ac1
Jerry Hollywhite <macbookair@MacBooks-MacBook-Air-2.local>
2025-12-21 23:55:23 +0700

    Allow Google Drive and Unsplash images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'doc-0k-0o-docs.googleusercontent.com', // Drive often redirects here
+      }
+    ],
+  },
 };
 
 export default nextConfig;
